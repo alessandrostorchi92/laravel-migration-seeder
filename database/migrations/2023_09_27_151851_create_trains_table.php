73c90ed84df5fd21e727582ebd28b89a0fd6c25b
alessandrostorchi92 <storchi.alle@gmail.com>
2023-09-27 18:20:36 +0200

    Creation tables

--- database/migrations/2023_09_27_151851_create_trains_table.php
+++ database/migrations/2023_09_27_151851_create_trains_table.php
@@ -11,10 +11,27 @@ return new class extends Migration
      *
      * @return void
      */
-    public function up()
-    {
+    public function up(){
+
+        // Di base tutte le tabelle sono nullable(false) perciò non possono essere lasciate vuote. 
+
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('Company', 50);
+            $table->string('Stazione_di_partenza', 50);
+            $table->string('Stazione_di_arrivo', 50);
+
+            // Per visualizzare l'orario in questo formato(HH:MM) imposto la tipologia string 
+            $table->string('Orario_di_partenza', 5);
+            $table->string('Orario_di_arrivo', 5);
+
+            $table->string('Codice_treno', 5);
+            $table->string('Numero_carrozze',100);
+
+            // I dati di queste tabelle devono essere di default false perchè devo specificare il loro valore 
+            $table->boolean("in_orario")->nullable(true);
+            $table->boolean("cancellato")->default(0);
+
             $table->timestamps();
         });
     }
